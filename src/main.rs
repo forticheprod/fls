a58f8475ab8cfe8ad50f18b1452a65a4c9f64639
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-02-01 19:03:51 +0100

    implement current directory

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,23 @@ use std::fs::ReadDir;
 use std::path::Path;
 use std::{env, fs};
 
+fn get_current_working_dir() -> String {
+    let file_path = env::current_dir();
+    match file_path {
+        Ok(file) => file.to_string_lossy().to_string(),
+        Err(_) => panic!("Current Dir does not exist"),
+    }
+}
+
 fn get_path() -> String {
     let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    let _ = Path::new(file_path).exists();
+    let file_path: String = if args.len() > 1 {
+        args.get(1).unwrap().to_string()
+    } else {
+        get_current_working_dir()
+    };
+
+    let _ = Path::new(&file_path).exists();
     file_path.to_string()
 }
 
