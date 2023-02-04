39d2fcf3ad501af4a45920b6fffdecd5ea40fd8f
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-02-04 14:02:46 +0100

    write new benches

--- benches/small.rs
+++ benches/small.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use fil_ls::{parse_dir, run};
 
 fn parse_and_run() {
-    let paths: Vec<String> = parse_dir("./samples".to_string());
+    let paths: Vec<String> = parse_dir("./samples/small".to_string());
     let _results: Vec<String> = run(paths);
 }
 
