07d257a5fa0d288e71b1135cb0bd310c4d905e24
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-02-10 16:35:00 +0100

    simplify args

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,12 @@
 use framels::{parse_dir, run};
 use std::env;
+use std::ffi::{OsStr, OsString};
 use std::path::Path;
 
-fn get_current_working_dir() -> String {
-    let file_path = env::current_dir();
-    match file_path {
-        Ok(file) => file.to_string_lossy().to_string(),
-        Err(_) => panic!("Current Dir does not exist"),
-    }
-}
-
-fn get_path() -> String {
-    let args: Vec<String> = env::args().collect();
-    let file_path: String = if args.len() > 1 {
-        args.get(1).unwrap().to_string()
-    } else {
-        get_current_working_dir()
-    };
-
-    let _ = Path::new(&file_path).exists();
-    file_path.to_string()
-}
-
 fn main() {
-    let paths: Vec<String> = parse_dir(get_path());
+    let args: Vec<_> = env::args_os().skip(1).collect();
+    let input_paths = args.last();
+    let paths: Vec<String> = parse_dir(input_paths);
     let results: Vec<String> = run(paths);
     println!("{}", results.join("\n"))
 }
