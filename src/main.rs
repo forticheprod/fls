8d597b5abcbebfc66b2af5e493c9a73929dcb78e
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-02-10 19:41:29 +0100

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+use clap::Parser;
 use framels::{parse_dir, run};
-use std::env;
-use std::ffi::{OsStr, OsString};
-use std::path::Path;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    list: bool,
+
+    #[arg(default_value_t = String::from("."), last = true)]
+    path: String,
+}
 
 fn main() {
-    let args: Vec<_> = env::args_os().skip(1).collect();
-    let input_paths = args.last();
-    let paths: Vec<String> = parse_dir(input_paths);
+    let args = Args::parse();
+    println!("{}", &args.path);
+    let paths: Vec<String> = parse_dir(args.path);
     let results: Vec<String> = run(paths);
     println!("{}", results.join("\n"))
 }
