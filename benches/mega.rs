dbc981e21f937d0f3c618f13e1e598a03f506f1e
Philippe Llerena <charlesbrenten@hotmail.com>
2023-02-12 11:51:45 +0000

    add clippy ignore

--- benches/mega.rs
+++ benches/mega.rs
@@ -18,6 +18,7 @@ fn parse_and_run() {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
+    #[allow(clippy::redundant_closure)]
     c.bench_function("mega", |b| b.iter(|| parse_and_run()));
 }
 criterion_group!(name=benches;config = Criterion::default().significance_level(0.1).sample_size(10); targets=criterion_benchmark);
