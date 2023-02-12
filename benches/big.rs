41aa243d22ab966ac2c68aa26226cad5a89ed046
Philippe Llerena <charlesbrenten@hotmail.com>
2023-02-12 11:22:47 +0000

    clean bench

--- benches/big.rs
+++ benches/big.rs
@@ -7,7 +7,7 @@ fn parse_and_run() {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("big", |b| b.iter(|| parse_and_run()));
+    c.bench_function("big", |b| parse_and_run());
 }
 criterion_group!(name=benches;config = Criterion::default().significance_level(0.1).sample_size(100); targets=criterion_benchmark);
 criterion_main!(benches);
