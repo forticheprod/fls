31cabacf8c451acb59973a71cb9e3345f391ce44
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-06-18 15:53:31 +0200

    add localized error message

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,15 +1,25 @@
 use assert_cmd::prelude::*; // Add methods on commands
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
+use sys_locale::get_locale;
+
+fn get_error_missing_folder_localize() -> String {
+    let locale: String = get_locale().unwrap_or_else(|| String::from("en-US"));
+    if locale == "fr-FR" {
+        "Le chemin d’accès spécifié est introuvable.".to_string()
+    } else {
+        "No such file or directory".to_string()
+    }
+}
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin("fls")?;
+    let mut cmd: Command = Command::cargo_bin("fls")?;
 
     cmd.arg("--").arg("./samples/toto/");
     cmd.assert()
         .failure()
-        .stderr(predicate::str::contains("No such file or directory"));
+        .stderr(predicate::str::contains(get_error_missing_folder_localize()));
 
     Ok(())
 }
