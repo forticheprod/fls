567746f0224b72815bd44461de7f1cc5207e6a5c
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-06-18 15:39:54 +0200

    add cli test

---
+++ tests/cli.rs
@@ -0,0 +1,15 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fls")?;
+
+    cmd.arg("--").arg("./samples/toto/");
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "Le chemin d’accès spécifié est introuvable.",
+    ));
+
+    Ok(())
+}
