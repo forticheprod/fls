475fbd3460dd29cbeaf3f89b4ed4ec7a05ab070a
Philippe Llerena <philippe.llerena@forticheprod.com>
2023-09-17 12:46:15 +0200

    bump version in cli also

--- tests/cli.rs
+++ tests/cli.rs
@@ -42,7 +42,7 @@ fn cli_version() -> Result<(), Box<dyn std::error::Error>> {
     cmd.arg("-V");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("framels 0.4.0"));
+        .stdout(predicate::str::contains("framels 0.4.2"));
 
     Ok(())
 }
