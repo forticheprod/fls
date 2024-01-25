b86a785881d0615e598ab07ebf7e41e96956d23b
Philippe Llerena <philippe.llerena@forticheprod.com>
2024-01-25 10:23:57 +0100

    Refactor exr_metadata.rs and update version in cli.rs

--- src/exr_metadata.rs
+++ src/exr_metadata.rs
@@ -5,21 +5,14 @@ use exr::prelude::*;
 /// ## Todo
 ///
 /// This function need to be refactored to generate a cleaner output.
+/// https://github.com/forticheprod/fls/issues/236
 ///
 /// ## Description
 ///
 /// Read the metadata of an exr file
 ///
 /// ## Example
-///
-/// ```rust
-/// use exr::prelude::*;
-/// use exr_metadata::read_meta;
-///
-/// let source = "./samples/big/RenderPass_Beauty_1_00000.exr".to_string();
-/// let expect = "./samples/big/RenderPass_Beauty_1_00000.exr layer #0 size:Vec2(320, 143); channels:ChannelList { list: [ChannelDescription { name: exr::Text(\"A\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"B\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"G\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"Plane_Beauty.A\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"Plane_Beauty.B\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"Plane_Beauty.G\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"Plane_Beauty.R\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"R\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"Spheres_Beauty.A\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"Spheres_Beauty.B\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"Spheres_Beauty.G\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }, ChannelDescription { name: exr::Text(\"Spheres_Beauty.R\"), sample_type: F16, quantize_linearly: false, sampling: Vec2(1, 1) }], bytes_per_pixel: 24, uniform_sample_type: Some(F16) }".to_string();
-/// assert_eq!(expect, read_meta(source));
-/// }
+/// The module is private no need to document it.
 pub fn read_meta(path: String) -> String {
     let msg = format!("run path `{}` to generate the required file", &path);
     let meta_data = MetaData::read_from_file(
