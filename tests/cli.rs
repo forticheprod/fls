b0db133b95e8d6f0b215f897eb9fe254dbb3a43c
Philippe Llerena <philippe.llerena@gmail.com>
2024-07-29 10:16:52 +0200

    Bump framels version to 0.7.6

--- tests/cli.rs
+++ tests/cli.rs
@@ -42,7 +42,7 @@ fn cli_version() -> Result<(), Box<dyn std::error::Error>> {
     cmd.arg("-V");
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("framels 0.7.5"));
+        .stdout(predicate::str::contains("framels 0.7.6"));
 
     Ok(())
 }
