f3aae8c310e420c974157282dc98d456bb8bba77
Serhat Baykal <sbaykal@pm.me>
2024-11-11 19:47:03 +0100

    impl `Frames` to hold all the datas of a sequences for easily use them later

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 //!
 //! The key concept of the library is to pack frames sequences in a new
 //! filename like `toto.***.jpg@158-179`. It use a regex to extract the frame
-//! number and pack the frames. The regex is `(?x)(.*)(\.|_)(?P<frames>\d{2,9})\.(\w{2,5})$`.
+//! number and pack the frames. The regex is `(?x)(?P<name>.*)(?P<sep>\.|_)(?P<frames>\d{2,9})\.(?P<ext>\w{2,5})$`.
 //! It results in the limitations of only:
 //!
 //! - `.` or `_` as a separator between the filename and the frame number.
@@ -94,7 +94,8 @@
 //!   let in_paths: Paths = parse_dir("./samples/small");
 //!
 //!  // Generate results based on arguments
-//! let results: String = basic_listing(in_paths, false).get_paths().join("\n");
+//! let results: String = basic_listing(in_paths, false,
+//!     "{name}{sep}{padding}.{ext}@{first_frame}-{last_frame}".to_string()).get_paths().join("\n");
 //!
 //! println!("{}", results)
 //! }
@@ -110,6 +111,7 @@ use regex::{Captures, Regex};
 use std::collections::HashMap;
 use std::fs;
 use std::{clone::Clone, path::PathBuf};
+use strfmt::strfmt;
 
 /// # parse_dir
 /// List files and directories in the targeted directory, take a `String` as
@@ -141,52 +143,185 @@ pub fn recursive_dir(input_path: &str) -> Paths {
 }
 
 /// This function extract the matching group based on regex already compile to
-/// a tuple of string. For exemple toto.458.jpg should return
-/// (toto.***.jpg, 458)
+/// a tuple of (file_padded:String, frame_number:String, hashmap<re_group:String, capture:String>)
+/// For example toto.459.jpg should return:
+/// (toto.***.jpg, 458, [("name", "toto"), ("sep", "."), ("frames", "458"), ("ext", "jpg")])
 #[inline(always)]
-fn extract_regex(x: &str) -> (String, String) {
+fn extract_regex(x: &str) -> (String, Option<HashMap<String, String>>) {
     lazy_static! {
-        static ref RE_FLS: Regex = Regex::new(r"(?x)(?P<name>.*)(?P<sep>\.|_)(?P<frames>\d{2,9})\.(?P<ext>\w{2,5})$")
-            .expect("Can't compile regex");
+        static ref RE_FLS: Regex =
+            Regex::new(r"(?x)(?P<name>.*)(?P<sep>\.|_)(?P<frames>\d{2,9})\.(?P<ext>\w{2,5})$")
+                .expect("Can't compile regex");
     }
     let result_caps: Option<Captures> = RE_FLS.captures(&x);
     match result_caps {
-        None => (x.to_string(), "None".to_string()),
-        caps_wrap => {
-            let caps = caps_wrap.unwrap();
+        None => (x.to_string(), None),
+        Some(caps) => {
+            let hashmap: HashMap<String, String> = RE_FLS
+                .capture_names()
+                .flatten()
+                .filter_map(|n| {
+                    caps.name(n)
+                        .map(|m| (n.to_string(), m.as_str().to_string()))
+                })
+                .collect();
             (
                 x.replace(&caps["frames"], &"*".repeat(caps["frames"].len())),
-                caps["frames"].to_string(),
+                Some(hashmap),
             )
         }
     }
 }
 
+/// A struct representing a collection of frames with associated information.
+///
+/// # Fields
+///
+/// * `name` - The name of the frame collection.
+/// * `sep` - A separator string used in the frame collection.
+/// * `frames` - A vector containing the frame data as integers.
+/// * `ext` - The file extension associated with the frames.
+/// * `padding` - The amount of padding to be applied to the frames.
+#[derive(Debug)]
+struct Frames {
+    name: String,
+    sep: String,
+    frames: Vec<i32>,
+    ext: String,
+    padding: usize,
+}
+
+impl Frames {
+    /// Creates a `Frames` struct from a `HashMap`.
+    ///
+    /// # Parameters
+    ///
+    /// * `_hashmap` - A reference to a `HashMap<String, String>` containing the frame details.
+    /// name, sep, frames, ext, padding
+    /// # Returns
+    ///
+    /// A `Frames` struct populated with the values from the `HashMap`.
+    fn from_hashmap(_hashmap: &HashMap<String, String>) -> Self {
+        let name = _hashmap.get("name").unwrap_or(&"None".to_string()).clone();
+        let sep = _hashmap.get("sep").unwrap_or(&"None".to_string()).clone();
+        let frame = _hashmap
+            .get("frames")
+            .unwrap_or(&"None".to_string())
+            .clone();
+        let frames = vec![frame.parse().unwrap_or(0)];
+        let ext = _hashmap.get("ext").unwrap_or(&"None".to_string()).clone();
+        let padding = frame.len();
+
+        Frames {
+            name,
+            sep,
+            frames,
+            ext,
+            padding,
+        }
+    }
+
+    /// Returns the minimum value in `self.frames`.
+    ///
+    /// # Returns
+    ///
+    /// An `Option<i32>` containing the minimum value in `self.frames`, or `None` if `self.frames` is empty.
+    fn first_frame(&self) -> Option<i32> {
+        self.frames.iter().min().copied()
+    }
+
+    /// Returns the maximum value in `self.frames`.
+    ///
+    /// # Returns
+    ///
+    /// An `Option<i32>` containing the maximum value in `self.frames`, or `None` if `self.frames` is empty.
+    fn last_frame(&self) -> Option<i32> {
+        self.frames.iter().max().copied()
+    }
+
+
+    /// Converts the `Frames` struct to a `HashMap` of string values.
+    ///
+    /// This was implemented to be used with `strfmt::strfmt`
+    ///
+    /// # Returns
+    ///
+    /// A `HashMap<String, String>` containing the following key-value pairs:
+    ///
+    /// * `"name"` - The name of the frame.
+    /// * `"sep"` - The separator used in the frame.
+    /// * `"ext"` - The extension of the frame.
+    /// * `"first_frame"` - The first frame number as a string, or `"None"` if `self.frames` is empty.
+    /// * `"last_frame"` - The last frame number as a string, or `"None"` if `self.frames` is empty.
+    /// * `"padding"` - A string of asterisks (`'*'`) representing the padding length.
+    fn to_hashmap(&self) -> HashMap<String, String> {
+        let mut map = HashMap::new();
+        map.insert("name".to_string(), self.name.clone());
+        map.insert("sep".to_string(), self.sep.clone());
+        map.insert("ext".to_string(), self.ext.clone());
+        map.insert(
+            "first_frame".to_string(),
+            self.first_frame()
+                .map_or("None".to_string(), |f| f.to_string()),
+        );
+        map.insert(
+            "last_frame".to_string(),
+            self.last_frame()
+                .map_or("None".to_string(), |f| f.to_string()),
+        );
+        map.insert(
+            "padding".to_string(),
+            std::iter::repeat('*').take(self.padding.into()).collect(),
+        );
+        map
+    }
+}
+
+impl PartialEq for Frames {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+            && self.sep == other.sep
+            && self.frames == other.frames
+            && self.ext == other.ext
+            && self.padding == other.padding
+    }
+}
+
 /// Parse the result of a vector of string. This function use HashMap to pack
 /// filename removed from the frame value.
-fn parse_result(dir_scan: Paths, multithreaded: bool) -> HashMap<String, Vec<String>> {
+fn parse_result(dir_scan: Paths, multithreaded: bool) -> HashMap<String, Option<Frames>> {
     // Optimisation over PAR_THRESHOLD value, the parsing of the frame list
     // used rayon lib to paralelize the work. Result depends a lot from the
     // cpu number of thread may be put in a config file
     const PAR_THRESHOLD: usize = 100000;
-    let extracted: Vec<(String, String)> = if (dir_scan.len() > PAR_THRESHOLD) | multithreaded {
-        dir_scan
-            .par_iter()
-            .map(|path| extract_regex(path.to_str().unwrap()))
-            .collect()
-    } else {
-        dir_scan
-            .iter()
-            .map(|path| extract_regex(path.to_str().unwrap()))
-            .collect()
-    };
-    let mut paths_dict: HashMap<String, Vec<String>> = HashMap::with_capacity(extracted.len());
+    let extracted: Vec<(String, Option<HashMap<String, String>>)> =
+        if (dir_scan.len() > PAR_THRESHOLD) | multithreaded {
+            dir_scan
+                .par_iter()
+                .map(|path| extract_regex(path.to_str().unwrap()))
+                .collect()
+        } else {
+            dir_scan
+                .iter()
+                .map(|path| extract_regex(path.to_str().unwrap()))
+                .collect()
+        };
+
+    let mut paths_dict: HashMap<String, Option<Frames>> = HashMap::with_capacity(extracted.len());
     for extraction in extracted {
-        let vec1: Vec<String> = vec![extraction.1.clone()];
         paths_dict
             .entry(extraction.0)
-            .and_modify(|value| (*value).push(extraction.1))
-            .or_insert(vec1);
+            .and_modify(|f| match f {
+                None => (),
+                Some(f) => match &extraction.1 {
+                    None => (),
+                    Some(t) => f.frames.push(t.get("frames").unwrap().parse().unwrap_or(0)),
+                },
+            })
+            .or_insert(match &extraction.1 {
+                None => None,
+                Some(t) => Some(Frames::from_hashmap(t)),
+            });
     }
     paths_dict
 }
@@ -208,42 +343,6 @@ fn group_continuity(data: &[isize]) -> Vec<Vec<isize>> {
     result.iter().map(|x| x.to_vec()).collect()
 }
 
-/// Basic function to:
-/// - convert vector of string into vector of isize
-/// - analyse the continuity
-/// - convert group of continuity into a concat string
-fn create_frame_string(value: Vec<String>) -> String {
-    let mut converted_vec_isize: Vec<isize> = value
-        .into_iter()
-        .map(|x| x.parse().expect("Failed to parse integer"))
-        .collect();
-    converted_vec_isize.sort();
-    let group_continuity: Vec<Vec<isize>> = group_continuity(&converted_vec_isize);
-    // Concatenation of continuity group in a string
-    group_continuity
-        .into_iter()
-        .map(|x| {
-            if x.len() == 1 {
-                x[0].to_string()
-            } else {
-                format!("{}-{}", x.first().unwrap(), x.last().unwrap())
-            }
-        })
-        .collect::<Vec<String>>()
-        .join(",")
-}
-
-/// This part of the code is not already used
-#[allow(dead_code)]
-fn concat_line(main_string: String, frame_string: String) -> String {
-    let buf: bool = false;
-    if buf {
-        let from: String = String::from("#####");
-        main_string.replace(&from, &frame_string)
-    } else {
-        format!("{}@{}", main_string, frame_string)
-    }
-}
 /// ## Basic listing of the library
 /// ### Description
 ///
@@ -280,20 +379,25 @@ fn concat_line(main_string: String, frame_string: String) -> String {
 ///     let in_paths: Paths = parse_dir("./samples/small");
 ///
 ///     // Generate results based on arguments
-///     let results: String = basic_listing(in_paths, false).get_paths().join("\n");
+///     let results: String = basic_listing(in_paths, false, "{name}{sep}{padding}.{ext}@{first_frame}-{last_frame}".to_string()).get_paths().join("\n");
 ///
 ///      println!("{}", results)
 /// }
 /// ```
-pub fn basic_listing(frames: Paths, multithreaded: bool) -> PathsPacked {
-    let frames_dict: HashMap<String, Vec<String>> = parse_result(frames, multithreaded);
+pub fn basic_listing(frames: Paths, multithreaded: bool, format: String) -> PathsPacked {
+    let frames_dict = parse_result(frames, multithreaded);
     let mut frames_list: Vec<String> = frames_dict
         .into_par_iter()
         .map(|(key, value)| {
-            if value[0] == "None" && value.len() == 1 {
-                key
-            } else {
-                format!("{}@{}", key, create_frame_string(value))
+            match value {
+                None => key,
+                Some(f) => match strfmt(&format, &f.to_hashmap()) {
+                    Ok(s) => s,
+                    Err(e) => {
+                        eprint!("Error formatting string: {}", e);
+                        String::new()
+                    }
+                },
             }
         })
         .collect();
@@ -345,18 +449,29 @@ fn get_exr_metada(root_path: &String, path: &String) -> String {
 /// ./samples/small/foo.exr    Not an exr
 /// ```
 pub fn extended_listing(root_path: String, frames: Paths, multithreaded: bool) -> PathsPacked {
-    let frames_dict: HashMap<String, Vec<String>> = parse_result(frames, multithreaded);
+    let frames_dict = parse_result(frames, multithreaded);
     let mut out_frames: PathsPacked = PathsPacked::new_empty();
     for (key, value) in frames_dict {
-        if value[0] == "None" && value.len() == 1 {
-            out_frames.push_metadata(get_exr_metada(&root_path, &key));
-            out_frames.push_paths(key.into());
-        } else {
-            let to = value.first().unwrap();
-            let from = String::from_utf8(vec![b'*'; to.len()]).unwrap();
-            let new_path = &key.replace(&from, to);
-            out_frames.push_metadata(get_exr_metada(&root_path, &new_path));
-            out_frames.push_paths(format!("{}@{}", key, create_frame_string(value)).into());
+        match value {
+            None => {
+                out_frames.push_metadata(get_exr_metada(&root_path, &key));
+                out_frames.push_paths(key.into())
+            }
+            Some(f) => {
+                let to = f.frames.first().unwrap();
+                let frame = format!("{:0zfill$}", to, zfill = f.padding);
+                let new_path = format!(
+                    "{name}{sep}{frame}.{ext}",
+                    name = f.name,
+                    sep = f.sep,
+                    ext = f.ext
+                );
+                dbg!(&new_path);
+                out_frames.push_metadata(get_exr_metada(&root_path, &new_path));
+                out_frames.push_paths(PathBuf::from(
+                    strfmt("{name}@{first_frame}-{last_frame}", &f.to_hashmap()).unwrap(),
+                ));
+            }
         }
     }
     out_frames
